56f07f3d455d8fe0b67d294af899e689eefb3be2
Dylan Osborn <dylanosborn@mines.edu>
2022-09-27 14:27:17 -0600

    Re-shaped mouth and eyes, adjusted x/y

--- src/Cat.java
+++ src/Cat.java
@@ -12,9 +12,9 @@ public class Cat {
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 10;
+	private static final int EYE_WIDTH = 20;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = HEAD_DIMENSION/4;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -39,8 +39,8 @@ public class Cat {
 	
 		// Draw the mouth
 		g2.setColor(Color.pink);
-		x = catX + MOUTH_X;
-		y = catY + MOUTH_Y;
+		x = catX + MOUTH_X - 3;
+		y = catY + MOUTH_Y - 1;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
